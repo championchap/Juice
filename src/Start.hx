212c39c09fdc7e56dc7daa53fa1464051f750e67
Erik Watson <championchap@gmail.com>
2014-07-31 16:44:18 +0100

    Added an timer that calls a loop function however quickly is specified.

--- src/Start.hx
+++ src/Start.hx
@@ -4,6 +4,6 @@ import juice.Juice;
 
 class Start {
     static function main() {
-    	var j:Juice = new Juice(720, 405);
+    	var j:Juice = new Juice(720, 405, 30);
     }
 }
--- src/juice/Juice.hx
+++ src/juice/Juice.hx
@@ -3,6 +3,9 @@ package juice;
 import js.Browser;
 import js.html.CanvasElement;
 import js.html.Document;
+import js.html.CanvasRenderingContext2D;
+
+import haxe.Timer;
 
 class Juice
 {
@@ -10,23 +13,32 @@ class Juice
 	private var doc:Document;
 	private var canvas:CanvasElement;
 
+	private var ctx:CanvasRenderingContext2D;
+
 	public function new(width:Int, height:Int, fps:Int = 60){
-		setup(width, height);
+		setup(width, height, fps);
 	}
 
-	private function setup(w:Int, h:Int):Void {
+	private function setup(w:Int, h:Int, fps:Int):Void {
 		doc = Browser.window.document;
 		canvas = doc.createCanvasElement();
 
 		canvas.width = w;
 		canvas.height = h;
 
-		var cnx = canvas.getContext2d();
+		ctx = canvas.getContext2d();
 
-		cnx.fillStyle = "#FF0000";
-		cnx.fillRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle = "#FF0000";
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 		doc.body.appendChild(canvas);
+
+		var timer:Timer = new Timer(Std.int((1 / fps)*1000));
+		timer.run = loop;
+	}
+
+	private function loop():Void {
+		
 	}
 
 	private function start():Void {
