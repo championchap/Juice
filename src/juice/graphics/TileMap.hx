8293c9c48a9cf8f8f0f2cc87b06b21c09f36dbb4
Erik Watson <championchap@gmail.com>
2014-08-04 02:27:04 +0100

    tweaking the TileMap setup

--- src/juice/graphics/TileMap.hx
+++ src/juice/graphics/TileMap.hx
@@ -9,15 +9,18 @@ class TileMap extends Image
 
 	public var tileWidth:Int;
 	public var tileHeight:Int;
-	public var columns:Int;
-	public var rows:Int;
+	public var tileData:Array<Int>;
 
-	public function new(texture:Texture, tileWidth:Int, tileHeight:Int, columns:Int, rows:Int){
+	public function new(texture:Texture, data:Array<Int>, tileWidth:Int, tileHeight:Int){
 		super(texture, new Rectangle(0, 0, tileWidth, tileHeight));
 
 		this.tileWidth = tileWidth;
 		this.tileHeight = tileHeight;
-		this.columns = columns;
-		this.rows = rows;
+
+		tileData = data;
+	}
+
+	override public function render():Void {
+
 	}
 }
