fe8f742db5d36bbd18015acf67f6fd647ffd6f1f
Erik Watson <championchap@gmail.com>
2014-08-04 02:10:38 +0100

    Padded out the TileMap constructor a bit

--- src/juice/graphics/TileMap.hx
+++ src/juice/graphics/TileMap.hx
@@ -6,7 +6,18 @@ import juice.utils.Rectangle;
 
 class TileMap extends Image
 {
-	public function new(texture:Texture, tileWidth:Int, tileHeight:Int){
+
+	public var tileWidth:Int;
+	public var tileHeight:Int;
+	public var columns:Int;
+	public var rows:Int;
+
+	public function new(texture:Texture, tileWidth:Int, tileHeight:Int, columns:Int, rows:Int){
 		super(texture, new Rectangle(0, 0, tileWidth, tileHeight));
+
+		this.tileWidth = tileWidth;
+		this.tileHeight = tileHeight;
+		this.columns = columns;
+		this.rows = rows;
 	}
 }
