9c24aaebc5b2fd94d0eaca0fdc8b90b47a335680
Erik Watson <championchap@gmail.com>
2014-08-05 05:15:40 +0100

    Added an Overlaps class

---
+++ src/juice/colliders/Overlaps.hx
@@ -0,0 +1,16 @@
+package juice.colliders;
+
+class Overlaps
+{
+	public function new(){
+		
+	}
+
+	public static function hitboxToHitbox():Bool {
+		return false;
+	}
+
+	public static function hitboxToGrid():Bool {
+		return false;
+	}
+}
