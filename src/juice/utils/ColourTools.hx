1ac2db7c41782883e8a22c896e34298876b6c3b6
Erik Watson <championchap@gmail.com>
2014-12-15 19:26:11 +0000

    Changed the default colour values

--- src/juice/utils/ColourTools.hx
+++ src/juice/utils/ColourTools.hx
@@ -2,18 +2,19 @@ package juice.utils;
 
 class ColourTools
 {
-
+	// These colours are taken from 
+	// http://www.keller.com/html-quickref/4a.html
 	public static inline var BLACK:String 	= "#000000";
 	public static inline var WHITE:String 	= "#FFFFFF";
-	public static inline var GREY:String 	= "#666666";
+	public static inline var GREY:String 	= "#808080";
 
 	public static inline var RED:String 	= "#FF0000";
-	public static inline var ORANGE:String 	= "#000000";
-	public static inline var YELLOW:String 	= "#000000";
-	public static inline var GREEN:String 	= "#00FF00";
+	public static inline var ORANGE:String 	= "#FFA500";
+	public static inline var YELLOW:String 	= "#FFFF00";
+	public static inline var GREEN:String 	= "#008000";
 	public static inline var BLUE:String 	= "#0000FF";
-	public static inline var INDIGO:String 	= "#000000";
-	public static inline var VIOLET:String 	= "#000000";
+	public static inline var INDIGO:String 	= "#4B0082";
+	public static inline var VIOLET:String 	= "#EE82EE";
 
 	public function new(){
 		
