3f1a261ac2f286f6f3281b603d97be34db3aa9ed
Erik Watson <championchap@gmail.com>
2014-12-15 05:08:31 +0000

    Made an empty Generic Pool class, fill it in later

---
+++ src/juice/utils/Pool.hx
@@ -0,0 +1,10 @@
+package juice.utils;
+
+// TODO: Make the Generic Pool work, shouldn't be too hard
+// Maybe shouldn't be in utils, but we can move it later 
+class Pool
+{
+	public function new(){
+		
+	}
+}
