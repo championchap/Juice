611e65714706709e3cfdecc9b828cf408776b606
Erik Watson <championchap@gmail.com>
2014-12-16 22:53:12 +0000

    Added a Base class, this will contain common functionality for things like Entity to extend.

---
+++ src/juice/core/Base.hx
@@ -0,0 +1,27 @@
+package juice.core;
+
+// A simple object that all other display objects inherit from 
+// defines common functionality 
+
+class Base
+{
+	public function new(){
+		
+	}
+
+	public function start():Void {
+
+	}
+
+	public function end():Void {
+
+	}
+
+	public function update():Void {
+
+	}
+
+	public function render():Void {
+		
+	}
+}
